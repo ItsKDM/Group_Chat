4093dfbbc15bd3849959977aa204c5046d2f0e25
ItsKDM <darshangupta.0602@gmail.com>
2023-08-03 17:12:49 +0530

    Task12: Superpowers of the Admin

--- router/groupRouter.js
+++ router/groupRouter.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const groupController = require("../controller/groupController");
 const Authentication = require("../middleware/auth");
 
-router.post("/createGroup", Authentication, groupController.createGroup);
-
-router.post("/addToGroup", Authentication, groupController.addToGroup);
-
 router.get("/getGroups", Authentication, groupController.getGroups);
+router.get("/groupMembers/:groupName", Authentication, groupController.groupMembers);
+
+router.post("/createGroup", Authentication, groupController.createGroup);
+router.post("/addToGroup", Authentication, groupController.addToGroup);
+router.post("/deleteFromGroup", Authentication, groupController.deleteFromGroup);
 
 module.exports = router;
